2ad32348bdd96651bb47e09f4930667e3de0ca7a
RISHI JHA <54244144+Rishi6229@users.noreply.github.com>
2021-06-13 08:40:10 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+//challenge 1:
+//connecting the button to function;
+function generateCat()
+{
+   var image = document.createElement('img');
+   var div = document.getElementById('flex-cat-gen'); 
+   image.src = "https://cdn2.thecatapi.com/images/d9t.jpg";
+   div.appendChild(image);
+}
+
+//challenge 3:
+
+function rpsGame(yourChoice){
+   console.log(yourChoice);
+
+}
